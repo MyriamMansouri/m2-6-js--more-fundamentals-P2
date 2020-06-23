c5961dfc3f094c269eadae238a21f463cfd6cf3d
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-23 14:13:27 -0400

    Exercise 2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,3 +3,22 @@
 
 const lateComers = ['Felicia', 'Charles', 'Terry'];
 const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+
+const printGuestOrder = (arrEarly, arrLate) => {
+    if (Array.isArray(arrEarly) && Array.isArray(arrLate)) {
+        const guestList = [...arrEarly, ...arrLate];
+        printList(guestList);
+        return guestList
+    }
+};
+
+
+const printList = (arr) => {
+    arr.forEach( el => {
+      const orderedEl = `${arr.indexOf(el) + 1}. ${el}`;
+      console.log(orderedEl);
+    })
+}
+  
+
+printGuestOrder(earlyBirds, lateComers)
