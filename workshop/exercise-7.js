9bbbd84bc1a597639594a0e6e3b9b9c0c7a1d7ae
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-23 16:07:48 -0400

    Exercise 7

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -9,8 +9,14 @@ let foodPairings = {
 };
 
 // createRecommendations function
-
+const createRecommendations = (obj) => {
+  return Object.keys(obj).map( el => `With ${el}, it is best to have ${obj[el]}`);
+}
 // printRecommendations function
-
+const printRecommendations = (arr) => {
+  arr.forEach( el => {
+    console.log(el);
+  })
+}
 // function call (done)
 printRecommendations(createRecommendations(foodPairings));
